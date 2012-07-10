52e9ce43c7fc6643f2fa28c3d8e31a7b4ffb16a5
Thomas <thomas@oxynum.fr>
2012-07-10 22:18:31 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oxynum2::Application.config.secret_token = 'fae5d70d9ed84c435555e93c1dc9925cedf2878601ccde4164965aa5ed1f2f4daf5eba93b1d3fcb8af5cbf0f3a550e571795c803bb8047980c35566680bd603d'
