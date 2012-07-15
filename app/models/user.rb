3c3e8d3b655304846490194b8d4cc98e7b9eb200
Thomas <thomas@oxynum.fr>
2012-07-15 17:49:27 +0200

    fix
    Signed-off-by: Thomas <thomas@oxynum.fr>

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :gender, :ip, :username, :fb_first_name, :fb_last_name, :location, :fb_id, :fb_status, :birthday
   has_many :dreams
   validates :username, :uniqueness => true, :length => {:minimum => 3, :maximum => 20}
-  validates :ip, :presence => true
-  validates :username, :presence => {:unless => Proc.new { |user| user.name.present? }} 
-  validates :name, :presence => {:unless => Proc.new { |user| user.username.present? }} 
-  validates :fb_id, :presence => {:if => Proc.new { |user| user.fb_first_name.present? }}
-  
+  validates :ip, :presence => true  
   def name
     if fb_first_name
       "#{fb_first_name} #{fb_last_name}"
