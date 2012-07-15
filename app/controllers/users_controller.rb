50e892d1cf7eadce8db155650b5fe82659184d31
Thomas <thomas@oxynum.fr>
2012-07-15 18:34:43 +0200

    notice added, fix bug
    Signed-off-by: Thomas <thomas@oxynum.fr>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,11 +16,20 @@ def create
   if split.size == 3
     @user.birthday = Date.new(split[2].to_i, split[0].to_i, split[1].to_i)
   end
-  @dream.user = @user
   @user.ip = request.env['REMOTE_ADDR']
+  saved = @user.save
+  if !saved
+    notice = 'Le rêve a été correctement créé'+' mais l\'utilisateur existait déja'
+    @dream.user = User.where(:ip => @user.ip, :username => @user.username).first
+    if !@dream.user 
+      format.html { redirect_to new_user_path, notice: 'L\'utilisateur existe déjà' }
+    end
+  end
+  @dream.user ||= @user
+  notice ||= 'Le rêve a été correctement créé'
   respond_to do |format|
-   if @user.save && @dream.save && @user.dreams.where("created_at >= ?",1.hour.ago).count <= 50
-      format.html { redirect_to dreams_path, notice: 'Le rêve a été correctement créé' }
+   if @dream.save && @user.dreams.where("created_at >= ?",1.hour.ago).count <= 50
+      format.html { redirect_to dreams_path, notice: notice }
       format.json { render json: dreams_path, status: :created, location: users_path }
    elsif @user.dreams.where("created_at >= ?",1.hour.ago).count > 50 
       format.html { redirect_to dreams_path, notice: 'Vous avez créé trop de rêves !!' }
